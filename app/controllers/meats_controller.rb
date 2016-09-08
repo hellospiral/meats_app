a300564d04ce3b531c45bed0cfd654d40d1edee4
Dylan Barnard and Matt Carlson <dylan+matt@>
2016-09-08 11:28:24 -0700

    Users can edit, view meat listings

--- app/controllers/meats_controller.rb
+++ app/controllers/meats_controller.rb
@@ -20,8 +20,22 @@ class MeatsController < ApplicationController
     @meat = Meat.find(params[:id])
   end
 
+  def edit
+    @category = Category.find(params[:category_id])
+    @meat = Meat.find(params[:id])
+  end
+
+  def update
+    @meat = Meat.find(params[:id])
+    if @meat.update(meat_params)
+      redirect_to category_path(@meat.category)
+    else
+      render :edit
+    end
+  end
+
   private
   def meat_params
-    params.require(:meat).permit(:name, :description, :image)
+    params.require(:meat).permit(:name, :description, :image, :weight, :price)
   end
 end
